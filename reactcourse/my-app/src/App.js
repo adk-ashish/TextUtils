5a8d9b98a7a53aadf1ed247a36e0f86919cf8726
ashish <adhsashish125@gmail.com>
2024-04-11 12:46:26 +0545

    video 5

---
+++ reactcourse/my-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+let name = "Ashish <h1> </h1>" 
+function App() {
+  return (
+  // <div className="blank">Lovely</div>
+  <>
+  <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+  </nav>
+  <div className="container">
+    <h1>Hello my name is {name}</h1>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut adipisci doloremque dolores voluptatibus, minima voluptates temporibus soluta dolorem doloribus pariatur nobis repellendus qui cumque necessitatibus molestias debitis quaerat fugiat odio?
+  </div>
+  <img src="" alt="" />
+  </>
+  );
+}
+
+export default App;
